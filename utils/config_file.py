5cf2c35041b2496b47b0a2b3df51df08761e462a
kekW <panicua12@gmail.com>
2024-09-30 23:17:52 +0300

    init/add: config_file.py>headers / user_agents.py

---
+++ utils/config_file.py
@@ -0,0 +1,7 @@
+# HTML request headers
+HEADERS = {
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Connection": "keep-alive",
+    "Referer": "https://www.google.com/",
+}
